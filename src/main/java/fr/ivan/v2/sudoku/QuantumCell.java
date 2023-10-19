eea52e1e4640f31d1551d1dcaf924d6e1700b135
Ivan <imbert@spideo.tv>
2023-10-19 16:55:53 +0200

    refacto: Start V2

--- src/main/java/fr/ivan/v2/sudoku/QuantumCell.java
+++ src/main/java/fr/ivan/v2/sudoku/QuantumCell.java
@@ -6,8 +6,6 @@ import fr.ivan.util.Cell;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.*;
